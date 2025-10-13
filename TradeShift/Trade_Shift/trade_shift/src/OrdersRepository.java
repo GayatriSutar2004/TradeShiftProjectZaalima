0ee618c5dd442fd423443a96d60435ed51ee99d4
Gayatri Sutar <gayatrisutar10170@gmail.com>
2025-10-13 19:24:44 +0530

    Delete TradeShift/Trade_Shift/trade_shift/src/OrdersRepository.java
    wrong folder of ordersrepository file

--- TradeShift/Trade_Shift/trade_shift/src/OrdersRepository.java
+++
@@ -1,6 +0,0 @@
-package com.miliproject.springboot.repository;
-
-import com.miliproject.springboot.entity.Orders;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrdersRepository extends JpaRepository<Orders, Long> {}
